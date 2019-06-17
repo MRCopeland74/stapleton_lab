a7b046bb8e6cdb941113b8af9eebc1ab57aab1d9
Michael Copeland <mrc5353@uncw.edu>
2019-06-17 14:32:42 -0400

    running vQTL

---
+++ vQTL/Stamp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J Perm_Manching      # Job name
+#SBATCH -e myjob.e%j       # Name of stderr error file
+#SBATCH -p normal          # Queue (partition) name
+#SBATCH -N 2               # Total # of nodes (must be 1 for serial)
+#SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
+#SBATCH -t 48:00:00        # Run time (hh:mm:ss)
+#SBATCH --mail-user=mrc5353@uncw.edu
+#SBATCH --mail-type=all    # Send email at begin and end of job
+
+# Other commands must follow all #SBATCH directives...
+Rscript --verbose ./MainvQTLcode.R> ./output.Rout
